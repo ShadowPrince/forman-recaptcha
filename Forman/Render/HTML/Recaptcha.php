30ebffb7f76b9759bdbde3e5383cd2b6efaee7f8
ShadowPrince <shadowprince47@gmail.com>
2013-10-15 23:17:01 +0300

    fixed deps

--- Forman/Render/HTML/Recaptcha.php
+++ Forman/Render/HTML/Recaptcha.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Forman Recaptcha plugin
+ *
+ * @author Vasiliy Horbachenko <shadow.prince@ya.ru>
+ * @copyright 2013 Vasiliy Horbachenko
+ * @version 0.1
+ * @package shadowprince/forman-recaptcha
+ *
+ */
 namespace Forman\Render\HTML;
 
 class Recaptcha extends HTMLElement {
